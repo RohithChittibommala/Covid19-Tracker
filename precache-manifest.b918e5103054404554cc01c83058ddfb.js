d33f21a3d90b1537a5a86111dedca4af163cad6e
Rohith ch <myselfrohith@outlook.com>
2020-06-27 20:22:05 +0530

    Updates

---
+++ precache-manifest.b918e5103054404554cc01c83058ddfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac39307e1b95fbfbda0feb836343ec1",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "699e71f360dde77817cf",
+    "url": "/Covid19-Tracker/static/css/main.f2acf9c6.chunk.css"
+  },
+  {
+    "revision": "a5c955edf525c930b497",
+    "url": "/Covid19-Tracker/static/js/2.177cd19c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-Tracker/static/js/2.177cd19c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "699e71f360dde77817cf",
+    "url": "/Covid19-Tracker/static/js/main.43c4cd14.chunk.js"
+  },
+  {
+    "revision": "64c63ea51f87e501e681",
+    "url": "/Covid19-Tracker/static/js/runtime-main.81719429.js"
+  }
+]);
